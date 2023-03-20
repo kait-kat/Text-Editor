6cef15aeba9a1df677fcbe5cb990b3da04d1a038
kait <kingkittykait@gmail.com>
2023-03-19 20:52:19 -0700

    install event listeners added and exported db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,24 @@ const initdb = async () =>
     },
   });
 
+export const putDb = async (content) => {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ id: 1, value: content });
+    const result = await request;
+    console.log('Data saved to the database', result);
+  };
 
-export const putDb = async (content) => console.error('putDb not implemented');
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');  
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log(result);
+  return result;
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,7 +1,22 @@
 const butInstall = document.getElementById('buttonInstall');
 
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {console.log('hit')
+console.log("event" + event)
+event.preventDefault();
+window.deferredPrompt = event;
+butInstall.classList.toggle('hidden', false);
+});
 
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
+});
 
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
